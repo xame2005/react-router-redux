c47198b248584dbc51c2beb7df308c560e5b5822
Samuel Gonzalez <xame2005@gmail.com>
2020-10-28 00:15:15 -0600

    [Created] Routes handler, basic use of Switch, Route and BrowserRouter component

---
+++ src/routes/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "../containers/Home";
+import Login from "../containers/login";
+import Register from "../containers/register";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
