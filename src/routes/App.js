def1765bda2bea9e58f181122c8f9491d581068d
Samuel Gonzalez <xame2005@gmail.com>
2020-10-29 00:36:43 -0600

    [CREATED] scaffolding, working with reducers and redux

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "../containers/Home";
-import Login from "../containers/login";
-import Register from "../containers/register";
+import Login from "../containers/Login";
+import Register from "../containers/Register";
+import NotFound from "../containers/NotFound";
+import Layout from "../components/Layout";
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
